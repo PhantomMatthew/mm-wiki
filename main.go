9793251045b68be990e656e3ff80b7bcdbdbcce7
Jiabei Yuan <jiabei.yuan@gmail.com>
2019-01-18 16:13:27 +0800

    update tls version to 1.1 to make PCI DSS compliance

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
 	_"mm-wiki/app"
 	_"github.com/astaxie/beego/session/memcache"
 	_"github.com/astaxie/beego/session/redis"
 	_"github.com/astaxie/beego/session/redis_cluster"
 	"github.com/astaxie/beego"
+	//_"github.com/astaxie/BeeApp/Server"
 )
 
 func main() {
+	beego.BeeApp.Server.TLSConfig = &tls.Config{ MinVersion: 0x0302}
 	beego.Run()
 }
